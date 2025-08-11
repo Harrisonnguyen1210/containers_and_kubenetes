1feea84301a91c6353a479504b858fd3fcab1ea9
Hung Nguyen <giahungftu2@gmail.com>
2025-08-11 22:08:43 +0300

    Config map

--- server-fetcher/app.js
+++ server-fetcher/app.js
@@ -4,16 +4,11 @@ import { logger } from "@hono/hono/logger";
 
 const app = new Hono();
 
+const message = Deno.env.get("WELCOME_MESSAGE") || "Hello world!";
+
 app.use("/*", cors());
 app.use("/*", logger());
 
-app.get("/", async (c) => {
-  const res = await fetch("http://minikube-demo-server-service:8000");
-  const data = await res.json();
-
-  return c.json({
-    message: `Fetched: ${data.message}`,
-  });
-});
+app.get("/", (c) => c.json({ message }));
 
 export default app;
